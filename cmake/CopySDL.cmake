78792fd026ce36ead34d224ed67cde5c288381de
SemihMT <SemihMT@users.noreply.github.com>
2024-11-27 23:48:20 +0100

    Updated cmake file to find the correct path to libsdl

--- cmake/CopySDL.cmake
+++ cmake/CopySDL.cmake
@@ -12,10 +12,19 @@ elseif(APPLE)
         set(SDL_SHARED_LIB "libSDL2-2.0d.dylib")
     endif()
 elseif(UNIX)
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
-        set(SDL_SHARED_LIB "libSDL2-2.0.so")
-    elseif(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-        set(SDL_SHARED_LIB "libSDL2-2.0d.so")
+    # Search for the libSDL2.so file using a find command
+    execute_process(
+        COMMAND find ${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE} -name "libSDL2*.so"
+        RESULT_VARIABLE FIND_RESULT
+        OUTPUT_VARIABLE FIND_OUTPUT
+        ERROR_VARIABLE FIND_ERROR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    # Check if the find command was successful
+    if(FIND_RESULT EQUAL 0 AND FIND_OUTPUT)
+        string(REGEX REPLACE "^.*" "" SDL_SHARED_LIB ${FIND_OUTPUT})
+    else()
+        message(FATAL_ERROR "SDL2 shared library not found in ${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE}")
     endif()
 else()
     message(FATAL_ERROR "Unsupported platform for SDL copy!")
@@ -33,6 +42,7 @@ endif()
 
 # Add a post-build command to copy the appropriate SDL library
 if(DEFINED SDL_SHARED_LIB)
+
     add_custom_command(TARGET Nexus_Game POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${SDL_SHARED_LIB}"
