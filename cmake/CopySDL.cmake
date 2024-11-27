3db7cffaf41d029231403bf82d6bca8137350f66
SemihMT <SemihMT@users.noreply.github.com>
2024-11-28 00:47:29 +0100

    updated Copy command

--- cmake/CopySDL.cmake
+++ cmake/CopySDL.cmake
@@ -33,19 +33,13 @@ endif()
 
 # Add a post-build command to copy the appropriate SDL library
 if(DEFINED SDL_SHARED_LIB)
-    if(DEFINED UNIX)
-        add_custom_command(TARGET Nexus_Game POST_BUILD
-            # First, use find to locate the shared library after build
-            COMMAND ${CMAKE_COMMAND} -E echo "Searching for SDL shared library..."
-            COMMAND find . -name "libSDL2*.so"
-            COMMENT "Searching and copying SDL shared library after build (Unix)"
-        )
-        message("Configured post-build step to copy SDL library: ${SDL_SHARED_LIB}")
-    endif()
     add_custom_command(TARGET Nexus_Game POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E echo "Target executable directory: $<TARGET_FILE_DIR:Nexus_Game>"
+        COMMAND ${CMAKE_COMMAND} -E echo "Source SDL library: ${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${SDL_SHARED_LIB}"
+        COMMAND ${CMAKE_COMMAND} -E echo "Destination for SDL library: $<TARGET_FILE_DIR:Nexus_Game>/${SDL_SHARED_LIB}"
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
-        "${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${SDL_SHARED_LIB}"
-        $<TARGET_FILE_DIR:Nexus_Game>/${SDL_SHARED_LIB}
+            "${SDL2_BINARY_DIR}/${CMAKE_BUILD_TYPE}/${SDL_SHARED_LIB}"
+            $<TARGET_FILE_DIR:Nexus_Game>/${SDL_SHARED_LIB}
     )
     message("Configured post-build step to copy SDL library: ${SDL_SHARED_LIB}")
 endif()
