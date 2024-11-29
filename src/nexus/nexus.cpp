bdcc912f4e7b10800c56482bdb86f93d6a16adad
SemihMT <SemihMT@users.noreply.github.com>
2024-11-29 09:47:03 +0100

    Removed unnecessary CXX flags from CMakeLists, added ASIO headers to nexus lib

--- src/nexus/nexus.cpp
+++ src/nexus/nexus.cpp
@@ -1,5 +1,16 @@
 #include "nexus.h"
 #include <iostream>
+
+// Windows version definition (versions of windows handle networking differently?)
+#ifdef _WIN32
+#define _WIN32_WINNT 0x0A00 // Windows 10
+#endif
+
+// ASIO
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
 namespace nxs
 {
     void PrintHello()
